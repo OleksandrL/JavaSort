eb9a8ac711e5e3c3640085997cb87976fdb389fe
unknown <Oleksandr_Lukash@EPUAKYIW0467.kyiv.epam.com>
2015-10-02 11:00:17 +0300

    Modify field names in SortingTest.java file

--- test/oleksandr_lukash/java/sort/SortingTest.java
+++ test/oleksandr_lukash/java/sort/SortingTest.java
@@ -14,23 +14,23 @@ import static org.junit.Assert.*;
  */
 public class SortingTest {
     
-    private static int[] input;
-    private static int[] input2;
-    private static int[] input3;
-    private static int[] input4;
-    private static int[] input5;
-    private static int[] input6;
-    private static int[] input7;
+    private static int[] unsortedArray;
+    private static int[] alreadySortedArray;
+    private static int[] reverseSortedArray;
+    private static int[] emptyArray;
+    private static int[] notInitializedArray;
+    private static int[] arrayWithOneElement;
+    private static int[] arrayWithTwoElements;
     
     @Before
     public void setUp() {
-        input = new int[]{23, 0, -5, 0, -8, 0, 25, 25, 2, 1};
-        input2 = new int[]{-67, -5, -2, 0, 3, 3, 4, 5, 120};
-        input3 = new int[]{120, 5, 4, 3, 3, 0, -2, -5, -67};
-        input4 = new int[0];
-        input5 = null;
-        input6 = new int[]{0};
-        input7 = new int[]{1, -2};
+        unsortedArray = new int[]{23, 0, -5, 0, -8, 0, 25, 25, 2, 1};
+        alreadySortedArray = new int[]{-67, -5, -2, 0, 3, 3, 4, 5, 120};
+        reverseSortedArray = new int[]{120, 5, 4, 3, 3, 0, -2, -5, -67};
+        emptyArray = new int[0];
+        notInitializedArray = null;
+        arrayWithOneElement = new int[]{0};
+        arrayWithTwoElements = new int[]{1, -2};
     }
 
     /**
@@ -39,34 +39,34 @@ public class SortingTest {
     @Test
     public void testSelectSort() {
         int[] expResult = new int[]{-8, -5, 0, 0, 0, 1, 2, 23, 25, 25};
-        Sorting instance = new Sorting(input);
+        Sorting instance = new Sorting(unsortedArray);
         assertArrayEquals(expResult, instance.selectSort());
         
         expResult = new int[]{-67, -5, -2, 0, 3, 3, 4, 5, 120};
-        instance = new Sorting(input2);
+        instance = new Sorting(alreadySortedArray);
         assertArrayEquals(expResult, instance.selectSort());
         
         expResult = new int[]{-67, -5, -2, 0, 3, 3, 4, 5, 120};
-        instance = new Sorting(input3);
+        instance = new Sorting(reverseSortedArray);
         assertArrayEquals(expResult, instance.selectSort());
         
         expResult = new int[0];
-        instance = new Sorting(input4);
+        instance = new Sorting(emptyArray);
         assertArrayEquals(expResult, instance.selectSort());
         
         expResult = new int[]{0};
-        instance = new Sorting(input6);
+        instance = new Sorting(arrayWithOneElement);
         assertArrayEquals(expResult, instance.selectSort());
         
         expResult = new int[]{-2, 1};
-        instance = new Sorting(input7);
+        instance = new Sorting(arrayWithTwoElements);
         assertArrayEquals(expResult, instance.selectSort());
     }
     
     @Test(expected = NullPointerException.class)
     public void nullTestSelectSort(){
         int[] expResult = null;
-        Sorting instance = new Sorting(input5);
+        Sorting instance = new Sorting(notInitializedArray);
         assertArrayEquals(expResult, instance.selectSort());
     }
 
@@ -76,34 +76,34 @@ public class SortingTest {
     @Test
     public void testBubbleSort() {
         int[] expResult = new int[]{-8, -5, 0, 0, 0, 1, 2, 23, 25, 25};
-        Sorting instance = new Sorting(input);
+        Sorting instance = new Sorting(unsortedArray);
         assertArrayEquals(expResult, instance.bubbleSort());
         
         expResult = new int[]{-67, -5, -2, 0, 3, 3, 4, 5, 120};
-        instance = new Sorting(input2);
+        instance = new Sorting(alreadySortedArray);
         assertArrayEquals(expResult, instance.bubbleSort());
         
         expResult = new int[]{-67, -5, -2, 0, 3, 3, 4, 5, 120};
-        instance = new Sorting(input3);
+        instance = new Sorting(reverseSortedArray);
         assertArrayEquals(expResult, instance.bubbleSort());
         
         expResult = new int[0];
-        instance = new Sorting(input4);
+        instance = new Sorting(emptyArray);
         assertArrayEquals(expResult, instance.bubbleSort());
         
         expResult = new int[]{0};
-        instance = new Sorting(input6);
+        instance = new Sorting(arrayWithOneElement);
         assertArrayEquals(expResult, instance.bubbleSort());
         
         expResult = new int[]{-2, 1};
-        instance = new Sorting(input7);
+        instance = new Sorting(arrayWithTwoElements);
         assertArrayEquals(expResult, instance.bubbleSort());
     }
     
     @Test(expected = NullPointerException.class)
     public void nullTestBubbleSort(){
         int[] expResult = null;
-        Sorting instance = new Sorting(input5);
+        Sorting instance = new Sorting(notInitializedArray);
         assertArrayEquals(expResult, instance.bubbleSort());
     }
 
@@ -113,34 +113,34 @@ public class SortingTest {
     @Test
     public void testShellSort() {
         int[] expResult = new int[]{-8, -5, 0, 0, 0, 1, 2, 23, 25, 25};
-        Sorting instance = new Sorting(input);
+        Sorting instance = new Sorting(unsortedArray);
         assertArrayEquals(expResult, instance.ShellSort());
         
         expResult = new int[]{-67, -5, -2, 0, 3, 3, 4, 5, 120};
-        instance = new Sorting(input2);
+        instance = new Sorting(alreadySortedArray);
         assertArrayEquals(expResult, instance.ShellSort());
         
         expResult = new int[]{-67, -5, -2, 0, 3, 3, 4, 5, 120};
-        instance = new Sorting(input3);
+        instance = new Sorting(reverseSortedArray);
         assertArrayEquals(expResult, instance.ShellSort());
         
         expResult = new int[0];
-        instance = new Sorting(input4);
+        instance = new Sorting(emptyArray);
         assertArrayEquals(expResult, instance.ShellSort());
         
         expResult = new int[]{0};
-        instance = new Sorting(input6);
+        instance = new Sorting(arrayWithOneElement);
         assertArrayEquals(expResult, instance.ShellSort());
         
         expResult = new int[]{-2, 1};
-        instance = new Sorting(input7);
+        instance = new Sorting(arrayWithTwoElements);
         assertArrayEquals(expResult, instance.ShellSort());
     }
     
     @Test(expected = NullPointerException.class)
     public void nullTestShellSort(){
         int[] expResult = null;
-        Sorting instance = new Sorting(input5);
+        Sorting instance = new Sorting(notInitializedArray);
         assertArrayEquals(expResult, instance.ShellSort());
     }
 }
